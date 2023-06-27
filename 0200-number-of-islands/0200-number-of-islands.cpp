ac76ede665e6c7f6c9c199064e1f7c07d7191688
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-06-27 06:58:16 +0530

    Time: 32 ms (97.09%), Space: 13.4 MB (38.49%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,56 +1,45 @@
 class Solution {
 public:
-        void visitElement(vector<vector<char>>& grid, vector<vector<int>>& visited, queue<pair<int, int>>& q, int nrow, int ncol)
+    bool isValid(int m, int n, int r, int c)
     {
-        int m = grid.size();
-        int n = grid[0].size();
-        if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol] == '1'
-                       && visited[nrow][ncol] == 0)
-                    {
-                        q.push(make_pair(nrow, ncol));
-                        visited[nrow][ncol] = 1;
-                    }
+        return r >=0 && r <m && c >=0 && c<n;
     }
-    
-    void bfs(vector<vector<char>>& grid, vector<vector<int>>& visited, int row, int col)
+    void dfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int row, int col)
     {
-        visited[row][col] = 1;
-       
-        
-        queue<pair<int, int>> q;
-        q.push(make_pair(row, col));
-        
-        while(!q.empty())
-        {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-
-            visitElement(grid, visited, q, r -1, c);
-            visitElement(grid, visited, q, r, c-1);
-            visitElement(grid, visited, q, r + 1, c);
-            visitElement(grid, visited, q, r, c + 1);
-
-        }
-    }
     
+     vis[row][col] = 1;   
+     int rowDelta[] = {-1, 0, 1, 0};
+     int colDelta[] = {0, -1, 0, 1};
+     int m = grid.size();
+     int n = grid[0].size();  
+        
+     for(int i=0;i<4;i++)
+     {
+         int r = row + rowDelta[i];
+         int c = col + colDelta[i];
+         if(isValid(m, n, r, c) && grid[r][c] == '1' && vis[r][c]==0)
+            dfs(grid, vis, r, c);
+     }
+        
+    }
     int numIslands(vector<vector<char>>& grid) {
         
-        int m = grid.size();
-        int n = grid[0].size();
-        int res = 0;
-        vector<vector<int>>visited(m, vector<int>(n, 0));
-        for(int i=0; i<m; i++)
+    int m = grid.size();
+    int n = grid[0].size();
+    
+    vector<vector<int>>vis(m, vector<int>(n, 0));
+    int res = 0;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
         {
-            for(int j=0; j<n; j++)
-            {   
-              if(grid[i][j] == '1' && visited[i][j] == 0)
-              {
-                  res++;
-                  bfs(grid, visited, i, j);
-              }
+            if(vis[i][j] == 0 && grid[i][j] == '1')
+            {
+                res++;
+                dfs(grid, vis, i, j);
             }
         }
+    }
     return res;
     }
 };
