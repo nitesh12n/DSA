d664d1b5db6942961d4c365ea660e2845a9c84a7
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-07-16 01:45:55 +0530

    Time: 331 ms (5.36%), Space: 86.3 MB (6.51%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
-    int func(int idx,string&s,char ch,vector<vector<int>>&dp){
-        int n=s.size();
-		//If our index idx is more than n then we will return 0 as no operation can be done
-        if(idx>=n){
+    
+    int flips(string& s, int i, int hasOne, vector<vector<int>>& dp)
+    {        
+        int keep= 1e7;
+        int flip= 1e7;
+        
+        if(i==s.size())
             return 0;
-        }
-		//If we have already visited the index and char we would simply return its value
-        if(dp[idx][ch-'0']!=-1){
-            return dp[idx][ch-'0'];
-        }
-		//If we have our prev char as '0' then we have 2 choices to toggle it or to move to next idx
-        int res1=1e9,res2=1e9;
-        if(ch=='0'){
-            if(s[idx]=='0'){
-                res1=func(idx+1,s,s[idx],dp);
-                res2=func(idx+1,s,'1',dp)+1;
+        
+        if(dp[i][hasOne]!= -1)
+            return dp[i][hasOne];
+        
+        if(s[i]=='0')
+        {
+            if(hasOne)
+                flip = 1 + flips(s, i+1, 1, dp);
+            else
+            {
+                keep = flips(s, i+1, 0, dp);
+                flip = 1 + flips(s, i+1,1, dp);     
             }
-            else{
-                res1=func(idx+1,s,s[idx],dp);
-                res2=func(idx+1,s,'0',dp)+1;
-            }
+            
         }
-		
-		//If we have our prev char as '1' then we will simply 
-		//toggling every '0' char and adding one operation to it
-        else{
-            if(s[idx]=='0'){
-                res1=func(idx+1,s,'1',dp)+1;
+        else
+        {
+            if(hasOne)
+                keep = flips(s, i+1, 1, dp);
+            else
+            {
+                keep = flips(s, i+1, 1, dp);
+                flip = 1 + flips(s, i+1, 0, dp);
             }
-            else{
-                res1=func(idx+1,s,s[idx],dp);
-            }
-        }
-        return dp[idx][ch-'0']=min(res1,res2);
+        }    
+    return dp[i][hasOne] = min(keep, flip);
     }
     
     int minFlipsMonoIncr(string s) {
-	//First we will create a 2d vector of size of string.size()*2
-        vector<vector<int>>dp(s.size(),vector<int>(2,-1));
-		//Now we will call our recursive function 
-        int ans=func(0,s,'0',dp);
-        return ans;
-        
+        vector<vector<int>>dp(s.size(), vector<int>(2, -1));
+        return flips(s, 0, 0, dp);
     }
 };
