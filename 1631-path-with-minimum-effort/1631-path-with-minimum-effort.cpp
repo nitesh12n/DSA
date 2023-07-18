fb39f36144b96670aaa98852d041655d83c986a3
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-07-18 22:50:20 +0530

    Time: 223 ms (29.13%), Space: 20 MB (61.89%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        
+        int rows = heights.size();
+        int columns = heights[0].size();
+        
+        priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>>pq;
+        
+        vector<vector<int>>effort(rows, vector<int>(columns, 1e6));
+        effort[0][0]=0;
+        pq.push({0,{0,0}});
+        
+        vector<int>deltaX = {-1,0,1,0};
+        vector<int>deltaY = {0,1,0,-1};
+        while(!pq.empty())
+        {
+            auto data = pq.top();
+            pq.pop();
+            int eff = data.first;
+            int x = data.second.first;
+            int y=data.second.second;
+            
+            for(int i=0;i<4;i++)
+            {
+                int newX = x+deltaX[i];
+                int newY = y+deltaY[i];
+                if(newX>=0 && newX<rows && newY>=0 && newY<columns)
+                {
+                    int eff = max(effort[x][y], abs(heights[newX][newY] - heights[x][y]));
+                    if(effort[newX][newY] > eff)
+                    {
+                        effort[newX][newY] = eff;
+                        pq.push({eff,{newX,newY}});
+                    }
+                }
+            }
+        }
+        return effort[rows-1][columns-1];
+    }
+};
