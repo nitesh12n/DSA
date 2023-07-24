a3181817d048cb1275e9795c3722e0ed5c0dbdfa
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-07-24 21:23:27 +0530

    Time: 12 ms (46.38%), Space: 13.5 MB (95.30%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -13,10 +13,7 @@ class Solution {
                 high = mid -1;
             }
             else if (nums[mid]<target)
-                low = mid + 1;
-            else
-                high = mid - 1;
-                
+                low = mid + 1;      
         }
     return firstOcc;
     }
@@ -32,8 +29,6 @@ class Solution {
                 lastOcc = mid;
                 low = mid +1;
             }
-            else if (nums[mid]<target)
-                low = mid + 1;
             else
                 high = mid - 1;
                 
