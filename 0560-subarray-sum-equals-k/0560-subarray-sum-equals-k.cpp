6ad620e856181f7fc37b32c883b1c542353a5fbf
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-08-02 18:03:32 +0530

    Time: 75 ms (96.21%), Space: 46.9 MB (32.09%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,12 +4,13 @@ public:
         
         unordered_map<int,int>m;
         int sum=0,res=0;
+        m[0]=1;
         for(int num : nums)
         {
             sum+=num;
             res+=m[sum-k];
-            if(sum == k)
-                res++;
+            //if(sum == k)
+             //   res++;
             m[sum]++;
         }
         return res;
