795eb52d3cacc0dcb640e74161838e557429d5ff
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-08-04 14:27:07 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (38.30%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -3,15 +3,11 @@ public:
     vector<int> getRow(int rowIndex) {
        
         vector<int>res;
-        for(int i=0; i<=rowIndex; i++)
+        long value=1;   
+        res.push_back(1);
+        for(int i=1; i<=rowIndex; i++)
         {
-            long value=1;            
-            for(int j=1; j<=i;j++)
-            {
-               value*=rowIndex+1-j;
-               value/=j;
-                
-            }
+            value=(value*(rowIndex-i+1))/i;
             res.push_back(value);
         }
         return res;
