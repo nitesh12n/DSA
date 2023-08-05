659a1b7734263e485718163682678f7d3740f427
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-08-05 16:10:41 +0530

    Time: 23 ms (44.79%), Space: 18 MB (28.11%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,32 +1,26 @@
 class Solution {
-private:
-    void combine(vector<int>&arr, int k, int i, vector<int>&temp, vector<vector<int>>&res)
+    void combine(int n, int k, int i, vector<int>& temp, vector<vector<int>>& res)
     {
-    
         if(k==0)
         {
             res.push_back(temp);
             return;
         }
-        if(i == arr.size())
+        if(i == n+1)
             return;
-    
-        //take
-        temp.push_back(arr[i]);
-        combine(arr, k-1, i+1, temp, res);
+        
+        combine(n, k, i+1, temp, res);
+        temp.push_back(i);
+        combine(n, k-1, i+1, temp, res);
         temp.pop_back();
-        combine(arr, k,i+1, temp, res);
     }
 public:
     vector<vector<int>> combine(int n, int k) {
-     
-    vector<int>arr;
-        
-        for(int i=1;i<=n;i++)
-            arr.push_back(i);
+       
         vector<int>temp;
         vector<vector<int>>res;
-        combine(arr, k, 0, temp, res);
+        
+        combine(n, k, 1, temp, res);
         return res;
     }
 };
