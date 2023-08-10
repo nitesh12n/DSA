9b8c0055896d0b8ea1218ddd9a6b8329a0f224ef
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-08-10 16:16:06 +0530

    Time: 568 ms (5.63%), Space: 8.4 MB (5.35%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,39 +1,30 @@
 class Solution {
-    int isPossible(vector<int>& nums, int k, int maxSum)
+    
+    int splitArray(vector<int>& nums, int k, int index, vector<vector<int>>& dp)
     {
-        int sum=0, splits=1;
+        if(k==0 and index < nums.size() or k> 0 and index == nums.size())
+            return INT_MAX;
+        if(k==0)
+            return 0;
         
-        for(auto num : nums)
+       if(dp[index][k] != -1)
+           return dp[index][k];
+        
+        int sum = 0, minSum=INT_MAX;
+        
+        for(int i=index; i < nums.size(); i++)
         {
-            if(num + sum <= maxSum)
-                sum+=num;
-            else
-            {
-                sum=num;
-                splits++;
-            }
+            sum+=nums[i];
+            int res = max(sum, splitArray(nums, k-1, i + 1, dp));   
+            minSum = min(res, minSum);
         }
-    return splits <= k;
+    
+    return dp[index][k] = minSum;
     }
+    
 public:
     int splitArray(vector<int>& nums, int k) {
-        int low=0, high=0, mid;
-        
-        for(auto num : nums)
-        {
-            low = max(low, num);
-            high+=num;
-        }
-        while(low <= high)
-        {
-            mid = low + (high-low)/2;
-            
-            if(isPossible(nums, k, mid))
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        return low;
-        
+     vector<vector<int>>dp(nums.size(), vector<int>(k + 1, -1));
+        return splitArray(nums, k, 0, dp);
     }
 };
