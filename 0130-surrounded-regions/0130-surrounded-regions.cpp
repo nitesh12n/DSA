f98c7a8cadf462f5b632b1c41312225f8b06287e
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-08-13 02:22:39 +0530

    Time: 14 ms (53.26%), Space: 10.4 MB (38.24%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,55 +1,53 @@
 class Solution {
-public:
-    
-    void dfs(int i, int j, vector<vector<int>>& vis, vector<vector<char>>& board)
+    vector<int> deltaX = {0,-1,0,1};
+    vector<int> deltaY = {-1,0,1,0};
+    bool isValid(int i, int j, int m, int n)
     {
-        vis[i][j] = 1;
-        int m = board.size();
-        int n = board[0].size();
-        
-        vector<int>cordx = {0, -1, 0, 1};
-        vector<int>cordy = {-1, 0, 1, 0};
-            
-        for(int k=0; k<4; k++)
-        {
-            int x = i + cordx[k];
-            int y = j + cordy[k];
-            if(x >=0 && x<m && y>=0 && y<n && board[x][y] == 'O' && vis[x][y] == 0)
-                dfs(x, y, vis, board);
-        } 
-        
+        return i>=0 and i<m and j>=0 and j<n;
     }
+    void dfs(vector<vector<char>>& board, vector<vector<int>>&vis, int i, int j)
+    {
+        if(board[i][j] != 'O' or vis[i][j] == 1)
+            return;
+
+        int m = board.size(), n = board[0].size();
+        vis[i][j] = 1;
+        for(int x=0; x < 4; x++)
+        {
+            int r = i + deltaX[x];
+            int c = j + deltaY[x];
+            
+            if(isValid(r, c, m, n) and board[r][c] == 'O' and vis[r][c] == 0)
+                dfs(board, vis, r, c);             
+        }
+    }
+    
+public:
     void solve(vector<vector<char>>& board) {
         
-        
-        int m = board.size();
-        int n = board[0].size();
+        int m = board.size(), n = board[0].size();
         vector<vector<int>>vis(m, vector<int>(n, 0));
-        for(int i = 0; i<m; i++)
-        {
-            if(board[i][0] == 'O')
-                dfs(i, 0, vis, board);
-            
-            if(board[i][n-1] == 'O')
-                dfs(i, n-1, vis, board);
-        }
         
-        for(int j = 0; j<n; j++)
-        {   
-            if(board[0][j] == 'O')
-                dfs(0, j, vis, board);
-            
-            if(board[m-1][j] == 'O')
-                dfs(m-1, j, vis, board);
-        }
+        //top row
+        for(int j=0; j<n; j++)
+            dfs(board, vis, 0,j);
+        //bottom row
+        for(int j=0; j<n; j++)
+            dfs(board, vis, m-1,j);
+        //left column
+        for(int i=1; i<m-1; i++)
+            dfs(board, vis, i,0);
+        //right column
+        for(int i=1; i<m-1; i++)
+            dfs(board, vis, i,n-1);
         
-        for(int i = 0; i<m; i++)
+        for(int i=0; i<m; i++)
         {
-            for(int j = 0; j<n; j++)
-            {
-                if(board[i][j] == 'O' && vis[i][j] ==0)
-                    board[i][j] = 'X';
-            }
-        } 
+          for(int j=0; j<n; j++)
+          {
+              if(board[i][j] == 'O' and vis[i][j] == 0)
+                  board[i][j] = 'X'; 
+          }
+        }
     }
 };
