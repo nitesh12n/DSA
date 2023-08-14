386339c2c5e0ca8a804d1889212a8692b5b81b25
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-08-14 22:45:34 +0530

    Time: 183 ms (89.37%), Space: 69 MB (58.90%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,30 @@
+typedef pair<int, pair<int, int>> pqtype;
+class Solution {
+    
+    struct customFunc {
+        bool operator()(const pqtype& a, const pqtype& b) {
+            return a.first < b.first;
+        }
+    };
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> res;
+        priority_queue<pqtype, vector<pqtype>, customFunc>pq;
+        for(auto point : points)
+        {
+            int x = point[0], y = point[1];
+            int dist = x*x + y*y;
+            pq.push({dist, {x, y}});  
+            if(pq.size() > k)
+                pq.pop();
+        }
+        
+        while(!pq.empty())
+        {
+            auto point = pq.top();
+            pq.pop();
+            res.push_back({point.second.first, point.second.second});
+        }
+    return res;
+    }
+};
