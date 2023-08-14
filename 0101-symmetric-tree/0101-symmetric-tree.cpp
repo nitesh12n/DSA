84810af54cba69dc1badcc81f06b0b9ea11c4d89
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-08-14 16:27:54 +0530

    Time: 0 ms (100.00%), Space: 16.5 MB (31.03%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -10,17 +10,21 @@
  * };
  */
 class Solution {
-    bool isSymmetric(TreeNode* root1, TreeNode* root2)
+    bool isSame(TreeNode* root1, TreeNode* root2)
     {
         if(root1 == nullptr and root2 == nullptr)
             return true;
-        if(root1 == nullptr || root2 == nullptr || root1->val != root2->val)
+        if(root1 == nullptr or root2 == nullptr or root1 ->val != root2->val)
             return false;
         
-        return isSymmetric(root1->left, root2->right) and isSymmetric(root1->right, root2->left);
+        return isSame(root1->left, root2->right) and isSame(root1->right, root2->left);
     }
 public:
     bool isSymmetric(TreeNode* root) {
-        return isSymmetric(root, root);
+        
+        if(root == nullptr)
+            return true;
+        
+        return isSame(root->left, root->right);
     }
 };
