17d025cd7ea550afc693f295369388895e7e1010
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-08-28 11:43:05 -0700

    Time: 23 ms (90.82%), Space: 14.8 MB (49.82%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    vector<int>deltaX = {0,-1,0,1};
+    vector<int>deltaY= {-1,0,1,0};
+    int isValid(int i, int j, int m, int n)
+    {
+        return i>=0 and i<m and j>=0 and j<n;
+    }
+public:
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        int INF = 2147483647;
+        int m = rooms.size();
+        int n = rooms[0].size();
+        
+        queue<pair<pair<int,int>,int>>q;
+        
+        
+        for(int i=0; i<m; i++)
+        {
+             for(int j=0; j<n; j++)
+             {
+                if(rooms[i][j] == 0)
+                        q.push({{i,j}, 0});
+             }  
+        }        
+        
+        while(!q.empty())
+        {
+            auto cell = q.front();
+            q.pop();
+            int i = cell.first.first;
+            int j = cell.first.second;
+            int dist = cell.second;
+            for(int x=0; x < 4; x++)
+            {
+                int r = i + deltaX[x];
+                int c = j + deltaY[x];
+
+                if(isValid(r, c, m, n) and rooms[r][c] == INF)
+                {
+                    q.push({{r, c}, dist + 1}); 
+                    rooms[r][c] = dist + 1;
+                }
+            }
+        }          
+    }
+};
