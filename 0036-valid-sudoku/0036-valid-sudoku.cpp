795eecf689baa51ef7d192e7ad8edd996c614642
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-08-29 15:24:24 -0700

    Time: 16 ms (80.14%), Space: 18.9 MB (57.74%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -18,32 +18,27 @@ public:
 
     bool isValidSudoku(vector<vector<char>>& board) {
        
-        //validate rows  
-        int n = 9;
+        //validate rows and columns  
         for(int i = 0; i < 9; i++)
         {
-            vector<int>count(n+1, 0);
+            vector<int>rows(10, 0);
+            vector<int>columns(10, 0);
+            
             for(int j = 0; j < 9; j++)
             {
-                if(board[i][j] != '.' and ++count[board[i][j] - '0'] > 1)
-                    return false;            
-            }
-        }
-        //validate columns  
-        for(int i = 0; i < 9; i++)
-        {
-            vector<int>count(n+1, 0);
-            for(int j = 0; j < 9; j++)
-            {
-                if(board[j][i] != '.' and ++count[board[j][i] - '0'] > 1)
-                    return false;            
+                //rows
+                if(board[i][j] != '.' and ++rows[board[i][j] - '0'] > 1)
+                    return false;
+                //columns
+                if(board[j][i] != '.' and ++columns[board[j][i] - '0'] > 1)
+                    return false;  
             }
         }
         
         //validate 3x3 
-        for(int i = 0; i < n; i+=3)
+        for(int i = 0; i < 9; i+=3)
         {
-            for(int j = 0; j < n; j+=3)
+            for(int j = 0; j < 9; j+=3)
             {
                 if(validate(board, i, j) == false)
                     return false;    
