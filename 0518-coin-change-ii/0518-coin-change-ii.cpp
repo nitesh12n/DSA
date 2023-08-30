aef36ff3779532c592283ec9028bf223a7f70b16
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-08-30 09:47:17 -0700

    Time: 224 ms (5.01%), Space: 18.9 MB (19.23%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,4 +1,24 @@
 class Solution {
+    int change2(int amount, int index, vector<int>& coins, vector<vector<int>>& dp) {
+        
+        if(amount == 0)
+            return 1;
+        
+        if(amount < 0 or index == coins.size())
+            return 0;
+        
+        if(dp[index][amount] != -1)
+            return dp[index][amount];
+        
+        int res = 0;
+        for(int i = index; i < coins.size(); i++)
+        {
+            if(amount >= coins[i])
+                res+= change2(amount - coins[i], i,  coins, dp);
+        }
+        
+        return dp[index][amount] = res;
+    }
     
     int change(int amount, int index, vector<int>& coins, vector<vector<int>>& dp) {
         
@@ -19,9 +39,10 @@ class Solution {
         
         return dp[index][amount] = take + notTake;
     }
+    
 public:
     int change(int amount, vector<int>& coins) {
         vector<vector<int>>dp(coins.size(), vector<int>(amount + 1, -1));
-        return change(amount, 0, coins, dp);
+        return change2(amount, 0, coins, dp);
     }
 };
