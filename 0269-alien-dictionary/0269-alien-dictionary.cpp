1ddcaf1170cdd74468014cf02573ccbb9581b044
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-09-03 21:20:17 -0700

    Time: 3 ms (79.08%), Space: 9.5 MB (76.56%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.cpp
+++ 0269-alien-dictionary/0269-alien-dictionary.cpp
@@ -1,19 +1,47 @@
 class Solution {
+    
+    vector<int>topoSort(vector<vector<int>>&adj, set<char>& s)
+    {
+        vector<int> indegree(adj.size(), 0);
+        queue<int> q;
+        for(int i = 0; i < adj.size(); i++)
+        {
+            for(int p : adj[i])
+                indegree[p]++;
+        }
+
+        for(int i =0;i<adj.size();i++)
+            if(indegree[i]==0 and s.contains(i + 'a'))
+                q.push(i);
+        
+        vector<int> res;
+        while(!q.empty())
+        {
+            int course = q.front();
+            q.pop();
+            res.push_back(course);
+            for(auto a : adj[course])
+            {
+                indegree[a]--;
+                if(indegree[a] == 0)
+                    q.push(a);
+            }
+        }
+    return res;
+    }
+    
 public:
     string alienOrder(vector<string>& words) {
         
-        map<char, int>m;
+        set<char>s;
         for(int i=0; i<words.size();i++)
         {
             for(auto ch: words[i])
-            {
-                if(m.find(ch)==m.end())
-                {
-                    m[ch]=0;
-                }
-            }
+                s.insert(ch);
         }
-        vector<vector<char>>adj(26);
+        
+        
+        vector<vector<int>>adj(26);
         for(int i=1; i<words.size();i++)
         {
             string s1 = words[i-1];
@@ -21,45 +49,27 @@ public:
             
             int j = 0, len = min(s1.size(), s2.size());
             
-            while(j < len && s1[j] == s2[j])
-                j++;
-            
-            if(j<len)
+            for(j = 0; j < len; j++)
             {
-                adj[s1[j]-'a'].push_back(s2[j]);   
-                m[s2[j]]++;
-            }     
-            else if(j < s1.size() and j == s2.size())
-                return "";
-        }
-        
-        queue<char>q;
-        string res;
-        for(auto it = m.begin(); it != m.end(); it++)
-        {
-            if(it->second==0)
-                q.push(it->first);
-        }
-        
-        while(!q.empty())
-        {
-            char l = q.front();
-            q.pop();
-            res+=l;
-            
-            
-            for(auto i : adj[l-'a'])
-            {
-                m[i]--;
-                if(m[i]==0)
-                    q.push(i);
+              if(s1[j] != s2[j])
+              {                
+                  adj[s1[j]-'a'].push_back(s2[j]-'a');
+                  break;
+              }
             }
+            if(s1.size() > s2.size() and j == len)
+                return "";
+            
         }
-        if(res.size() == m.size())
-            return res;  
-        return "";
+        vector<int> seq = topoSort(adj, s); 
+        string res = "";
+
+        if(seq.size() != s.size())
+             return res;
         
-        
+        for(auto it : seq)
+            res+= (it + 'a');
+        return res;
         
     }
 };
