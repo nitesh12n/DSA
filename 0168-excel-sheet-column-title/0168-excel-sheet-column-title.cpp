c6cdb5fab3f6c20e96f6a934982263ab5f18d4a3
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-09-04 10:51:18 -0700

    Time: 0 ms (100.00%), Space: 6 MB (51.38%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        
+       string res;
+        
+        
+        while(columnNumber)
+        {
+            columnNumber--;
+            res+= (char)(columnNumber%26 + 'A');
+            columnNumber/=26;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
