2e02e5a4ecff6842a53118b460bf0f009e604deb
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-09-08 22:05:41 -0700

    Time: 28 ms (40.34%), Space: 28.8 MB (9.69%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
         
-        int carry = 0, i = num.size()-1, operand1, operand2;
+        int carry = 0, i = num.size(), operand1, operand2;
         vector<int>res;
-        while(i >=0 or k)
+        while(--i >=0 or k)
         {    
             operand1 = i >=0 ? num[i] : 0;
             operand2 = k%10;
@@ -12,7 +12,6 @@ public:
             int result = operand1 + operand2 + carry;            
             res.push_back(result%10);
             carry = result/10;     
-            i--;
         }
         if(carry)
             res.push_back(carry);
