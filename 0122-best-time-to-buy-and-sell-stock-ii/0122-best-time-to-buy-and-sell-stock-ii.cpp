6c834993a064594cee37a17c988faaa7246b2521
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-09-09 15:05:40 -0700

    Time: 8 ms (45.75%), Space: 17.8 MB (7.89%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -7,14 +7,14 @@ public:
         if(dp[i][buy] != -1)
             return dp[i][buy];
         if(buy == 0)
-            return dp[i][buy] = max(prices[i] + profit(prices, i + 1, 1, dp), profit(prices, i + 1, 0, dp));
+            return dp[i][buy] = max(profit(prices, i + 1, 1, dp) - prices[i], profit(prices, i + 1, 0, dp));
         else
-            return dp[i][buy] = max(profit(prices, i + 1, 1, dp), profit(prices, i +1, 0, dp) - prices[i]);
+            return dp[i][buy] = max(profit(prices, i + 1, 1, dp), profit(prices, i +1, 0, dp) + prices[i]);
     }
     int maxProfit(vector<int>& prices) {
         
         vector<vector<int>>dp(prices.size() + 1, vector<int>(2, -1));
-        return profit(prices, 0, 1, dp);
+        return profit(prices, 0, 0, dp);
         //vector<vector<int>>dp(prices.size() + 1, vector<int>(2, 0));
         //dp[prices.size()][0] = dp[prices.size()][1] = 0;
         for(int i = prices.size() - 1; i >=0; i--)
@@ -22,9 +22,9 @@ public:
             for(int buy = 0; buy < 2; buy++)
             {
                 if(buy == 0)
-                    dp[i][buy] = max(prices[i] + dp[i + 1][1], dp[i+1][0]);
+                    dp[i][buy] = max(prices[i] + dp[i + 1][1], dp[i + 1][0]);
                 else
-                    dp[i][buy] = max(dp[i + 1][1], dp[i +1][0] - prices[i]);
+                    dp[i][buy] = max(dp[i + 1][1], dp[i + 1][0] - prices[i]);
             }
         }
         return dp[0][1];
