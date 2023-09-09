debae4ac0ccaab498d76d5c03d5a2281acc1ab6e
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-09-08 23:27:06 -0700

    Time: 5 ms (29.74%), Space: 6.9 MB (9.35%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        unordered_map<char, int>m;
+        
+        for(auto ch : s)
+            m[ch]++;
+        
+        for(auto ch : t)
+        {
+            if(m.find(ch) == m.end())
+                return ch;
+            m[ch]--;
+        }
+        for(auto it : m)
+            if(it.second < 0)
+                return it.first;
+    return '\0';
+    }
+};
