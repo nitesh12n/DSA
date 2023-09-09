ce40987d3d15966ed22d8bb9658a0f185db9d447
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-09-09 10:06:14 -0700

    Time: 41 ms (51.85%), Space: 23.5 MB (10.40%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.cpp
+++ 0554-brick-wall/0554-brick-wall.cpp
@@ -2,26 +2,32 @@ class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
         
-        
         unordered_map<int, int>m;
         int prefixSum;
+        int totalRowSum = 0;
+        int res = 0;
+        
+        for(int width : wall[0])
+            totalRowSum+=width;
+        
         for(auto row : wall)
         {
            prefixSum = 0;
-           for(auto width : row)
+           for(int i = 0; i < row.size() - 1; i++)
            {
-               prefixSum+=width;
+               prefixSum+= row[i];
                m[prefixSum]++;
+               res = max(res, m[prefixSum]);
            }
         }
         
-        int res = 0;
-        int totalRowSum = prefixSum;
-        for(auto it : m)
+        //int totalRowSum = prefixSum;
+        /*for(auto it : m)
         {
-            if(it.first != 0 and it.first != totalRowSum)
+            if(it.first != totalRowSum)
                 res = max(res, it.second);
         }
+        */
         return wall.size() - res;
     }
 };
