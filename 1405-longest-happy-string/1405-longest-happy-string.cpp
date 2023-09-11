dafc237fe6bf2157981fef418d6e3bb3c76e3789
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-09-11 16:09:37 -0700

    Time: 3 ms (24.04%), Space: 6.3 MB (13.00%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.cpp
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -1,69 +1,48 @@
 class Solution {
 public:
-    
-    
-    
-string longestDiverseString(int a, int b, int c) {
-    // max heap stores {no. of times we can use char, char}
-    priority_queue<pair<int, char>> pq;
-    
-    if(a > 0){
-        pq.push({a, 'a'});
-    }
-    
-    if(b > 0){
-        pq.push({b, 'b'});
-    }
-    
-    if(c > 0){
-        pq.push({c, 'c'});
-    }
-    
-    string ans;
-    while(pq.size() != 0){
-    // first maximum
-        char curL = pq.top().second;
-        int occL = pq.top().first;
+    string longestDiverseString(int a, int b, int c) {
+       
         
-        pq.pop();
+        priority_queue<pair<int, char>>pq;
         
-        if(ans.size() == 0 || ans.back() != curL){
-            int times = 0;
-            times = min(occL, 2);
-            for(int i=0; i<times; i++){
-                ans += curL;
-            }
-
-            occL -= times;
-            if(occL > 0){
-                pq.push({occL, curL});
-            }
-
-        }
-        else{  // second maximum 
-
-            // add one ocurance of char of second max freq if it exists.
-            if(pq.size() == 0){
-                break;
-            }
-
-            char curS = pq.top().second;
-            int occS = pq.top().first;
+        if(a > 0)
+            pq.push({a, 'a'});
+        if(b > 0)
+            pq.push({b, 'b'});
+        if(c > 0)
+            pq.push({c, 'c'});
+                
+        string res;
+        
+        while(!pq.empty())
+        {
+            auto ch1 = pq.top();
             pq.pop();
-
-            ans += curS;
-            occS--;
-            if(occS > 0){
-                pq.push({occS, curS});
+            if(res.empty() or res.back() != ch1.second)
+            {
+                int size = min(ch1.first, 2);
+                string s(size, ch1.second);
+                res+= s;
+                ch1.first-= size;
             }
-
-            // return to the pq the max char we popped out in if clause:
-            pq.push({occL, curL});            
-
+            else
+            {
+                if(!pq.empty())
+                {
+                    auto ch2 = pq.top();
+                    pq.pop();
+                    res+= ch2.second;
+                    ch2.first--;
+                    if(ch2.first > 0)
+                        pq.push(ch2);
+                }
+                else
+                    return res;
+            }          
+            if(ch1.first > 0)
+                pq.push(ch1);
         }
         
+        return res;
     }
- 
-    return ans;
-}
 };
