e600708b4c90038750e31d6247c076d7f19f9430
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-09-13 18:42:03 -0700

    Time: 48 ms (10.55%), Space: 18 MB (9.72%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,49 @@
+class Solution {
+    
+    vector<string> dfs(map<string, vector<string>>& graph, string airport, vector<string>& temp, unordered_map<string, int>&freq, int edges)
+    {
+        if(temp.size() == edges + 1)
+            return temp;
+        vector<string>res;
+        
+        for(auto it : graph[airport])
+        {
+            string key = airport + it;
+            
+            if(freq[key] > 0)
+            {
+                freq[key]--;
+                temp.push_back(it);
+                res = dfs(graph, it, temp, freq, edges);
+                if(res.size() > 0)
+                    return res;
+                temp.pop_back();
+                freq[key]++;
+            }
+        }
+    return {};
+    }
+    
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+       
+        map<string, vector<string>>graph;
+        unordered_map<string, int>freq;
+        for(auto ticket : tickets)
+        {
+            graph[ticket[0]].push_back(ticket[1]);
+            freq[ticket[0] + ticket[1]]++;
+        }
+        
+        for(auto it : graph)
+        {
+            auto v = it.second;
+            sort(v.begin(), v.end());
+            graph[it.first] = v;
+        }
+        
+        vector<string>temp;
+        temp.push_back("JFK");
+        return dfs(graph, "JFK", temp, freq, tickets.size());
+    }
+};
