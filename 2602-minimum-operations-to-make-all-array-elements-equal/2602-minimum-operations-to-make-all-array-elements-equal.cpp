cd559524c213abef02d952b88e1498972f5d28eb
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-12-25 12:11:05 -0800

    Time: 160 ms (96.93%), Space: 84.8 MB (18.09%) - LeetHub

---
+++ 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    
+    int getIndex(vector<int>& nums, int target)
+    {
+        int low = 0, res = -1, high = nums.size() - 1;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            if(nums[mid] <= target)
+            {
+                res = mid;
+                low = mid + 1;
+            }
+            else
+                high = mid - 1;
+        }
+        return res;
+    }
+    
+    
+public:
+    vector<long long> minOperations(vector<int>& nums, vector<int>& queries) {
+        
+        vector<long long> res;
+        vector<long long>prefixSum;
+        long long totalSum = 0;
+        sort(nums.begin(), nums.end());
+        long long n = nums.size();
+        for(auto num : nums)
+        {
+            totalSum += num;
+            prefixSum.push_back(totalSum);
+        }       
+        for(auto query : queries)
+        {
+            long long index = getIndex(nums, query);
+            long long operations = 0;
+            if(index == -1)
+                operations = totalSum - query*n; 
+            else
+                operations = (query*(index + 1) - prefixSum[index]) + ((totalSum - prefixSum[index]) - query*(n - index - 1));
+
+            res.push_back(operations);
+        }
+        return res;
+    }
+};
