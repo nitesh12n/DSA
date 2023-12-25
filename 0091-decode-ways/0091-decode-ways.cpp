91652e562260a3ec6b2ddc0ee24b4e5562ce0ff5
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-12-24 20:49:50 -0800

    Time: 3 ms (47.64%), Space: 8 MB (11.91%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -3,10 +3,11 @@ class Solution {
 private: 
     char decode(string s)
     {
-        int num = stoi(s);
         if(s[0]=='0')
             return '\0';
-        if(num>0 and num<=26)
+        
+        int num = stoi(s);
+        if(num > 0 and num <= 26)
             return 'A'+num-1;
         return '\0';
     }
@@ -18,15 +19,15 @@ private:
         if(dp[index] != -1)
             return dp[index];
         char ch = decode(s.substr(index,1));
-        int ans=0;
+        int ans = 0;
         if(ch != '\0')
-            ans=numDecodings(s, index +1, dp);
+            ans= numDecodings(s, index + 1, dp);
         if(index+1 <s.size())
         {
             
             ch = decode(s.substr(index,2));
             if(ch != '\0')
-                ans+=numDecodings(s, index +2, dp);
+                ans+= numDecodings(s, index + 2, dp);
         }  
         return dp[index]= ans;
     }
