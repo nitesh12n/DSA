d2821ff09c2b64d59970586ca21c8784674ea29b
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-12-25 12:11:47 -0800

    Time: 169 ms (84.64%), Space: 84.9 MB (11.60%) - LeetHub

--- 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
+++ 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
@@ -32,6 +32,7 @@ public:
             totalSum += num;
             prefixSum.push_back(totalSum);
         }       
+        
         for(auto query : queries)
         {
             long long index = getIndex(nums, query);
@@ -39,7 +40,7 @@ public:
             if(index == -1)
                 operations = totalSum - query*n; 
             else
-                operations = (query*(index + 1) - prefixSum[index]) + ((totalSum - prefixSum[index]) - query*(n - index - 1));
+                operations = (query*(index + 1) - prefixSum[index]) + (totalSum - prefixSum[index]) - query*(n - index - 1);
 
             res.push_back(operations);
         }
