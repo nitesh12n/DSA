38abee61ede4f243e1449ca9b05b6ac759dbfb0f
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-12-28 08:31:15 -0800

    Time: 1326 ms (5.62%), Space: 307.7 MB (19.10%) - LeetHub

---
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    unordered_set<int>num = {1, 9, 99};
+    int getLength(string& s, char lastChar, int lastCharCount, int index, int k, unordered_map<int, int>& dp)
+    {
+        if( k < 0)
+            return 1e7;
+        if(index == s.size())
+            return 0;
+        
+        //string key = to_string(index) + "," + lastChar + "," + to_string(lastCharCount) + "," + to_string(k);
+        int key = index * 101 * 27 * 101 + (lastChar - 'a') * 101 * 101 + lastCharCount * 101 + k;
+        if(dp.find(key) != dp.end())
+            return dp[key];
+        
+        //delete
+        int d = getLength(s, lastChar, lastCharCount, index + 1, k - 1, dp);
+        
+        
+        //keep
+        int keep = 0;
+        
+        if(lastChar == s[index])
+            keep = getLength(s, lastChar, lastCharCount + 1, index + 1, k, dp) + (num.find(lastCharCount) != num.end() ? 1: 0);
+        else
+            keep = getLength(s, s[index], 1, index + 1, k, dp) + 1;
+        
+        return dp[key] = min(d, keep);
+    }
+    
+public:
+    int getLengthOfOptimalCompression(string s, int k) {
+        unordered_map<int, int> dp;
+        return getLength(s, '0', 0, 0, k, dp);
+    }
+};
