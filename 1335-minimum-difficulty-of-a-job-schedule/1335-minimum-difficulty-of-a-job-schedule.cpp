b1f30cf9fabff66340f6085ecbe620c3ac3ca240
Nitesh Gupta <niteshgupta1996@gmail.com>
2023-12-28 17:12:39 -0800

    Time: 301 ms (5.06%), Space: 8.2 MB (33.73%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    
+    
+    int minDifficulty(vector<int>& jobDifficulty, int index, int d, vector<vector<int>>&dp)
+    {
+        int difficulty = 0;
+        int res = INT_MAX;
+        if(index > jobDifficulty.size())
+            return 0;
+        
+        if(index == jobDifficulty.size() and d > 0)
+            return res;
+        
+        if(d == 0)
+        {
+            for(int i = index; i < jobDifficulty.size(); i++)
+                difficulty = max(difficulty, jobDifficulty[i]);
+            return dp[index][d] = difficulty;
+        }
+
+        
+        if(dp[index][d] != -1)
+            return dp[index][d];
+
+        for(int i = index; i < jobDifficulty.size(); i++)
+        {
+            difficulty = max(difficulty, jobDifficulty[i]);
+            int val = minDifficulty(jobDifficulty, i + 1, d - 1, dp);
+            if(val != INT_MAX)
+                res = min(res, val + difficulty);
+        }
+        return dp[index][d] = res;    
+    }
+    
+    
+public:
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        
+        if(d > jobDifficulty.size())
+            return -1;
+        
+        vector<vector<int>>dp(jobDifficulty.size() + 1, vector<int>(d + 1, -1));
+        int res =  minDifficulty(jobDifficulty, 0, d, dp);
+        if(res == INT_MAX)
+            return -1;
+        return res;
+    }
+};
