c8831ddc2c2424e17ef54f0e4ae48627b8aac68e
Nitesh Gupta <niteshgupta1996@gmail.com>
2024-01-01 14:06:54 -0800

    Time: 18 ms (56.58%), Space: 10.9 MB (14.47%) - LeetHub

---
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    
+    int getDistance(vector<int>& worker, vector<int>& bike) {
+        return abs(worker[0] - bike[0]) + abs(worker[1] - bike[1]);
+    }
+    
+    int assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes, int index, string& bikeMask, unordered_map<string, int>&dp)
+    {
+        if(index == workers.size())
+            return 0;
+        
+        string key = to_string(index) + "," + bikeMask;
+        if(dp.find(key) != dp.end())
+            return dp[key];
+        
+        int res = INT_MAX;
+        for(int i = 0; i < bikeMask.size(); i++)
+        {
+            if(bikeMask[i] == '0')
+            {
+                bikeMask[i] = '1';
+                int val = assignBikes(workers, bikes, index + 1, bikeMask, dp);
+                bikeMask[i] = '0';      
+                if(val != INT_MAX)
+                    res = min(res, val + getDistance(workers[index], bikes[i]));
+            }
+        }
+        return dp[key] = res;
+    }
+    
+public:
+    int assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes)     {
+        string bikeMask(bikes.size(), '0');
+        unordered_map<string, int>dp;
+        return   assignBikes(workers, bikes, 0, bikeMask, dp); 
+    }
+};
