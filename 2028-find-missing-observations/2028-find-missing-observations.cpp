0ec6d899dfa57bd8872c8abe1f9270481fcbeb51
Nitesh Gupta <niteshgupta1996@gmail.com>
2024-01-06 18:36:58 -0800

    Time: 135 ms (17.16%), Space: 124.1 MB (24.25%) - LeetHub

---
+++ 2028-find-missing-observations/2028-find-missing-observations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+     
+        
+        int m = rolls.size(), sum = 0;
+        
+        for(auto roll : rolls)
+            sum+=roll;
+        
+        int missingSum = mean*(n + m) - sum;
+        int minValue = missingSum/n, extra = missingSum%n;
+        if(minValue > 6 or minValue < 1)
+            return {};
+        vector<int>res;
+
+        for(int i = 0; i < n; i++)
+        {
+            res.push_back(minValue);
+            if(extra)
+                res[i]++, extra--;
+            if(res[i] > 6)
+                return {};
+            
+        }
+        return res;
+    }
+};
