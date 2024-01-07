cb6a2b373e7598de98aa9309ca367124744999c5
Nitesh Gupta <niteshgupta1996@gmail.com>
2024-01-06 22:53:03 -0800

    Time: 11 ms (64.08%), Space: 16.3 MB (54.49%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        
-        char minChar = 'z' + 1;
-        
-        
+                
         for(auto letter : letters)
         {
-            if(letter > target and letter - target < minChar - target)
-                minChar = letter;
+            if(letter > target)
+                return letter;
         }
-        return minChar == ('z' + 1) ? letters[0] : minChar;
+        return letters[0];
     }
 };
