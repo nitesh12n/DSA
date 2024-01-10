4b55623cd582232c9e5e3066102514d18071a45e
Nitesh Gupta <niteshgupta1996@gmail.com>
2024-01-10 14:03:40 -0800

    Time: 0 ms (100.00%), Space: 6.6 MB (42.02%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        
+        int size = palindrome.size();
+        if(size <= 1)
+            return {};
+        
+        int index = -1;
+        for(int i = 0; i < size/2; i++)
+        {
+            if(palindrome[i] != 'a')
+            {
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+        palindrome[size - 1] = 'b';
+        return palindrome;
+        
+    }
+};
