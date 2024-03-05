ba4799c5f25b657b240d2c212a86a30946b9bffc
Nitesh Gupta <niteshgupta1996@gmail.com>
2024-03-04 16:04:18 -0800

    Time: 553 ms (7.02%), Space: 351.1 MB (7.05%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,6 +1,5 @@
 class Solution {
-    vector<vector<int>> dp;
-    int lcs(string word1, string word2, int p1, int p2) {
+    int lcs(string word1, string word2, int p1, int p2, vector<vector<int>>& dp) {
         
         if(p1<0 && p2<0)
             return 0;
@@ -11,18 +10,18 @@ class Solution {
         if(p2<0){
             return p1+1;
         }
-        if(dp[p1][p2]!=0)
+        if(dp[p1][p2]!=-1)
             return dp[p1][p2];
         
         char ch1 = word1[p1];
         char ch2 = word2[p2];
         if(ch1==ch2){
-            dp[p1][p2] = lcs(word1, word2, p1-1, p2-1);
+            dp[p1][p2] = lcs(word1, word2, p1-1, p2-1, dp);
             return dp[p1][p2];
         }
         else {
-            int insert = lcs(word1, word2, p1, p2-1);
-            int del = lcs(word1, word2, p1-1, p2);
+            int insert = lcs(word1, word2, p1, p2-1, dp);
+            int del = lcs(word1, word2, p1-1, p2, dp);
             dp[p1][p2] = 1 + min(insert, del);
             return dp[p1][p2];
         }
@@ -30,7 +29,7 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         int len1 = word1.size(), len2 = word2.size();
-        dp.resize(len1 + 1, std::vector<int>(len2 + 1, 0));
-        return lcs(word1, word2, len1-1, len2-1);
+        vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, -1));
+        return lcs(word1, word2, len1-1, len2-1, dp);
     }
 };
