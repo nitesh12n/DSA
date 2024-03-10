9ffc560c429008dce1ecff0d249934ea9513b579
Nitesh Gupta <niteshgupta1996@gmail.com>
2024-03-09 21:43:49 -0800

    Time: 36 ms (69.67%), Space: 16.6 MB (61.27%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -9,31 +9,12 @@ class Solution:
         if not root:
             return []
         
-        currRow, currCol = 0, 0
-        
         d = defaultdict(list)
-
-#         def inorder(root, row, col):
-#             if not root:
-#                 return
-            
-#             d[col].append(root.val)
-            
-#             if root.left:
-#                 inorder(root.left, row + 1, col - 1)
-                
-#             if root.right:
-#                 inorder(root.right, row + 1, col + 1)
-                
-#         inorder(root, 0, 0)
-        
-#         nd = dict(sorted(d.items()))
 
         queue = [(root, 0, 0)]
     
         while queue:
             node, row, col = queue.pop(0)
-            # print(node.val)
             
             d[col].append(node.val)
             
